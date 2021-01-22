a12a8ffaf684bfd810d074bc07f4bf9a8fcef2ad
김영현 <kimyh03160@naver.com>
2021-01-22 19:07:30 +0900

    create router

---
+++ frontend/src/router/index.js
@@ -0,0 +1,28 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import HelloWorld from '../components/HelloWorld.vue';
+
+Vue.use(VueRouter);
+
+const routes = [
+    //path: 
+    //name: 
+    //component:
+    //한 쌍으로 넣어주기.
+    {
+        path : '/',
+        name : 'HelloWorld',
+        component : HelloWorld
+    },
+    
+    // concat을 통해서 routes 추가할 계획.
+    // 배열을 추가한다고 생각하면 됨.
+]
+
+const router = new VueRouter({
+    mode:'history',
+    base : process.env.BASE_URL, // /
+    routes,
+});
+
+export default router;
