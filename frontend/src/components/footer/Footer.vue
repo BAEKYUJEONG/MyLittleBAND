8ca408fc43c568f192658a18028385d156ccb9dc
엄재웅 <zzang6535@gmail.com>
2021-01-29 10:55:47 +0900

    기능: footer 디자인 수정
    footer에 팀원, 팀 이름
    추후에 소개, 카피라이트 문구 넣어서 더 꾸밀 예정
    팀원 이름 클릭시 자기의 블로그, github등으로 이동하도록 만듦

--- frontend/src/components/footer/Footer.vue
+++ frontend/src/components/footer/Footer.vue
@@ -1,15 +1,59 @@
 <template>
-  <v-footer fluid>
-    Footer Component
+  <v-footer color="#231E1A" padless>
+    <v-row justify="center" no-gutters>
+      <v-btn
+        v-for="member in Team"
+        :key="member"
+        color="white"
+        text
+        rounded
+        class="my-10 mx-10"
+        :href="member.url"
+      >
+        {{ member.name }}
+      </v-btn>
+      <v-col class="lighten-2 py-4 text-center white--text" cols="12">
+        {{ new Date().getFullYear() }} — <strong>Team A408</strong>
+      </v-col>
+    </v-row>
   </v-footer>
 </template>
 
 <script>
 export default {
-
-}
+  data: () => {
+    return {
+      Team: [
+        {
+          name: "김영현",
+          url: "",
+        },
+        {
+          name: "박성우",
+          url: "",
+        },
+        {
+          name: "백유정",
+          url: "",
+        },
+        {
+          name: "신인섭",
+          url: "",
+        },
+        {
+          name: "엄재웅",
+          url: "https://github.com/eomjaewoong",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
-
+.row,
+.col {
+  color: white;
+  text-align: center;
+}
 </style>
