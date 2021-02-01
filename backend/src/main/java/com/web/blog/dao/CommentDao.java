96406c4a41f95228b3696562b5e1a1bea20669eb
YuJeong <qordbwjd96@gmail.com>
2021-02-01 20:43:32 +0900

    기능 : Comment 기능 수정 및 추가
    DTO, xml, DAO, Service, ServiceImpl 추가

--- backend/src/main/java/com/web/blog/dao/CommentDao.java
+++ backend/src/main/java/com/web/blog/dao/CommentDao.java
@@ -1,5 +1,12 @@
 package com.web.blog.dao;
 
-public class CommentDao {
-	
+import org.apache.ibatis.annotations.Mapper;
+
+import com.web.blog.dto.Comment;
+
+@Mapper
+public interface CommentDao {
+	public int insert(Comment comment) throws Exception;
+	public int update(Comment comment) throws Exception;
+	public int delete(String commentId) throws Exception;
 }
