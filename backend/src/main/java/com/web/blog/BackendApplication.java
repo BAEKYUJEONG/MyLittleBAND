c8e99e74e2824ed8fd61e56bbf8e22dc2583ea81
박성우 <ailwoo1@naver.com>
2021-02-09 08:00:05 +0000

    기능: 파일 업로드

--- backend/src/main/java/com/web/blog/BackendApplication.java
+++ backend/src/main/java/com/web/blog/BackendApplication.java
@@ -1,15 +1,18 @@
 package com.web.blog;
 
+
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @MapperScan(basePackages = {"com.web.blog.mapper"})
 @SpringBootApplication
-public class BackendApplication {
 
+public class BackendApplication{
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
-
 }
