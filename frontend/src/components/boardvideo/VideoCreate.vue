5f6039943292ec6f69f00d8dcf67b0e977b32e25
KimYH-KU <kimyh03160@naver.com>
2021-02-13 04:48:11 +0900

    리팩토링 : 비디오 리스트, 비디오 업로드, 비디오 화면 - 로그인 여부에 따른 글쓰기 & 영상 게시판 열람 수정 - 영상 업로드 틀 제작 - 필터 & 검색 관련해서 통신 틀 만듦

--- frontend/src/components/boardvideo/VideoCreate.vue
+++ frontend/src/components/boardvideo/VideoCreate.vue
@@ -1,43 +1,158 @@
 <template>
-    <!-- 파일 첨부 폼 -->
-    <v-main>
-      <v-file-input v-model="files" show-size label="File input"></v-file-input>
-      <v-btn @click="upload" color="primary">Upload</v-btn>
-      <p>File Name : {{ files.name }}</p>
-    </v-main>
+  <v-main>
+    <v-container>
+      <h1 style="margin: 10px auto">영상 업로드</h1>
+      <v-row>
+        <v-col
+          ><h2 style="margin: 20px auto" class="black--text">제목</h2></v-col
+        >
+        <v-col>
+          <v-text-field solo style="margin: 20px auto" v-model="board.title">
+          </v-text-field>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col
+          ><h2 style="margin: 20px auto" class="black--text">내용</h2></v-col
+        >
+        <v-col>
+          <v-textarea
+            solo
+            full-width
+            height="200"
+            style="margin: 20px auto"
+            v-model="board.content"
+          ></v-textarea>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col
+          ><h2 style="margin: 20px auto" class="black--text">썸네일</h2></v-col
+        >
+        <v-col>
+          <v-file-input
+            v-model="imgfiles"
+            show-size
+            label="File input"
+          ></v-file-input>
+          <p>File Name : {{ imgfiles.name }}</p>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col
+          ><h2 style="margin: 20px auto" class="black--text">영상</h2></v-col
+        >
+        <v-col>
+          <v-file-input
+            v-model="videofiles"
+            show-size
+            label="File input"
+          ></v-file-input>
+          <p>File Name : {{ videofiles.name }}</p>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col
+          ><h2 style="margin: 20px auto" class="black--text">밴드</h2></v-col
+        >
+        <v-col class="d-flex" cols="12" sm="6">
+          <v-select
+            v-model="select"
+            :items="bandlist"
+            item-text="name"
+            item-value="bandId"
+            label="밴드명"
+            solo
+            dense
+          ></v-select>
+        </v-col>
+      </v-row>
+
+      <v-row>
+        <v-col>
+          <v-btn color="blue" style="margin: 10px" @click="create()"
+            >작성완료</v-btn
+          >
+          <v-btn color="blue" style="margin: 10px" @click="list()"
+            >돌아가기</v-btn
+          >
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-main>
 </template>
 <script>
-import axios from 'axios';
+import axios from '@/axios/axios-common.js';
+
+// 필요한 거
+// 썸네일
+// 영상 소스
+// 영상 제목
+// 밴드.. 자신이 속한 밴드 리스트를 가져와야 함.
+// 영상 설명
 
 export default {
-  name: 'App',
-  components: {
+  created() {
+    this.getbandlist();
   },
-  data: () => ({
-    files: [],
-  }),
+  data() {
+    return {
+      select: '',
+      bandlist: [],
+      imgfiles: [],
+      videofiles: [],
+      board: {
+        title: '',
+        content: '',
+      },
+    };
+  },
   methods: {
     async upload() {
       // 여기서 파일 업로드에 대한 요청 백으로 보냄.
+    },
+    list() {
+      this.$router.push('/video');
+    },
+    async create() {
+      //공백이 존재하면 경고
+      if (
+        this.board.title == '' ||
+        this.board.content == '' ||
+        this.videofiles.name == '' ||
+        this.imgfiles.name == '' ||
+        this.select == ''
+      ) {
+        alert('빈칸이 존재합니다.');
+        return;
+      }
 
-      var fd = new FormData();
-      fd.append('files', this.files)
-      // 아래 요청은 파일 첨부에 대한 예시.
-      await axios.post('http://localhost:8080/upload',
-            fd, {
-              headers: {
-                'Content-Type': 'multipart/form-data'
-              }
-            }
-          ).then( response => {
-            console.log('성공');
-            console.log(response.data)
-          })
-          .catch(function () {
-            console.log('실패');
-          });
-    }
-  
-  }
+      let fd = new FormData();
+      fd.append('imgfiles', this.imgfiles);
+      fd.append('videofiles', this.videofiles);
+
+      //이 부분은 백 보고 수정해야 할듯.
+      await axios
+        .post('/video', fd, {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
+        })
+        .then((response) => {
+          console.log(response);
+          alert('업로드 하였습니다!');
+          this.$router.push('/video');
+        })
+        .catch(function() {
+          console.log('실패');
+        });
+    },
+    getbandlist() {
+      axios
+        .get('/band-list/' + this.memberid)
+        .then((response) => (this.bandlist = response.data.object))
+        .catch((exp) => alert(exp + '조회에 실패하였습니다.'));
+    },
+  },
 };
 </script>
