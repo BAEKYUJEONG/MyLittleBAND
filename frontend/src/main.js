c426913773b31e8e421abe22c857ea49610a564d
KimYH-KU <kimyh03160@naver.com>
2021-02-16 02:21:06 +0900

    기능 : 카카오 링크를 통한 밴드 멤버 초대 - 카카오 링크를 통해 밴드 멤버 초대 기능 추가

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,14 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store/store'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store/store';
 import vuetify from './plugins/vuetify';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+// 카카오 링크 사용
+window.Kakao.init('02edea6a07b8bac0a336b0e60add8969');
 
 new Vue({
-  render: h => h(App),
+  render: (h) => h(App),
   vuetify,
   router,
   store,
-}).$mount('#app')
+}).$mount('#app');
