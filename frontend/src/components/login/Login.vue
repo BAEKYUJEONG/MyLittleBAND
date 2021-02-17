b518348bf65979477292b7478ccb754099a3ca41
엄재웅 <zzang6535@gmail.com>
2021-02-17 12:50:04 +0900

    리팩토링: 로그인 모달 디자인 수정
    로그인 모달 디자인 좀 더 편하게 보이도록 수정함

--- frontend/src/components/login/Login.vue
+++ frontend/src/components/login/Login.vue
@@ -1,4 +1,4 @@
-<template >
+<template>
   <v-dialog
     v-model="dialog"
     persistent
@@ -6,22 +6,23 @@
     style="background-color: #feeeb0"
   >
     <template v-slot:activator="{ on, attrs }">
-      <v-btn text v-bind="attrs" v-on="on" :class="clr" class="mx-3">로그인</v-btn>
+      <v-btn text v-bind="attrs" v-on="on" :class="clr"
+        >로그인</v-btn
+      >
     </template>
     <v-card>
       <v-card-title>
-        <span class="headline">로그인</span>
-        <v-spacer />
-        <v-btn icon @click="dialog = false">
+        <v-btn icon @click="dialog = false" class="ml-auto mr-1">
           <v-icon>mdi-close</v-icon>
         </v-btn>
       </v-card-title>
+      <v-card-title class="headline justify-center">로그인</v-card-title>
       <v-card-text>
         <v-container>
           <v-row>
             <v-col cols="12">
               <v-text-field
-                label="Email*"
+                label="이메일"
                 placeholder="mylittle@band.com"
                 v-model="member.email"
                 required
@@ -29,7 +30,7 @@
             </v-col>
             <v-col cols="12">
               <v-text-field
-                label="Password*"
+                label="비밀번호"
                 type="password"
                 v-model="member.pw"
                 required
@@ -37,15 +38,20 @@
               ></v-text-field>
             </v-col>
             <v-col cols="12">
-              <v-btn color="success" @click="onSubmit">로그인</v-btn>
+              <v-btn color="success" block @click="onSubmit">로그인</v-btn>
             </v-col>
           </v-row>
         </v-container>
       </v-card-text>
       <v-card-actions>
-        <v-spacer></v-spacer>
-        <v-btn color="blue darken-1" text @click="signup()"> 회원가입 </v-btn>
-        <v-btn color="blue darken-1" text @click="findidpw()">
+        <v-col cols="7" class="text-end">아직 회원이 아니신가요?</v-col>
+        <v-btn col-4 color="blue darken-1" text @click="signup()">
+          회원가입
+        </v-btn>
+      </v-card-actions>
+      <v-card-actions class="pb-5">
+        <v-col cols="7" class="text-end">아이디 / 비밀번호를 잊으셨나요?</v-col>
+        <v-btn col-4 color="blue darken-1" text @click="findidpw()">
           아이디/비밀번호 찾기
         </v-btn>
       </v-card-actions>
@@ -54,8 +60,8 @@
 </template>
 
 <script>
-import { mapActions, mapGetters } from 'vuex' //vuex사용
-const MemberStore = 'MemberStore' //MemberStore 모듈 사용
+import { mapActions, mapGetters } from "vuex"; //vuex사용
+const MemberStore = "MemberStore"; //MemberStore 모듈 사용
 
 export default {
   data: () => {
@@ -68,16 +74,18 @@ export default {
     };
   },
   props: ["clr"],
-  computed:{
-    ...mapGetters(MemberStore,{//MemberStore 모듈 내 getters 사용
-      memberid : 'getMemberId',//memberid 변수에 getMemberId 리턴값 저장
-      manager : 'getManager'    //manager 변수에 getManager 리턴값 저장
-    })
+  computed: {
+    ...mapGetters(MemberStore, {
+      //MemberStore 모듈 내 getters 사용
+      memberid: "getMemberId", //memberid 변수에 getMemberId 리턴값 저장
+      manager: "getManager", //manager 변수에 getManager 리턴값 저장
+    }),
   },
   methods: {
-    ...mapActions(MemberStore,[//MemberStore 모듈 내 actions 사용
-        'Login'
-      ]),
+    ...mapActions(MemberStore, [
+      //MemberStore 모듈 내 actions 사용
+      "Login",
+    ]),
 
     onSubmit: function() {
       //공백이면
@@ -86,11 +94,11 @@ export default {
         return;
       }
       this.Login(this.member); //Login action 사용 (member를 payload로 보냄)
-      if(localStorage.getItem("access-token") != ""){
+      if (localStorage.getItem("access-token") != "") {
         console.log("회원번호: " + this.memberid);
-            this.$router.push("/");
-            this.dialog = false;
-      }else{
+        this.$router.push("/");
+        this.dialog = false;
+      } else {
         alert("아이디 혹은 비밀번호가 틀렸습니다!");
       }
     },
@@ -110,5 +118,4 @@ export default {
 };
 </script>
 
-<style>
-</style>
+<style></style>
