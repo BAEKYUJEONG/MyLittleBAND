baa429878233ab94bb682a0a562d4b3adb9d6505
KimYH-KU <kimyh03160@naver.com>
2021-02-18 03:04:49 +0900

    리팩토링: 스트리밍 화면 구성 변경 & 1 : N 테스트 - 방송하는 화면과, 보는 화면 나눠둠. - callee가 모두 접속한 뒤, caller 측에서 broadcast -> offer 버튼을 누른 후, callee가 watch를 클릭.

---
+++ frontend/src/components/manageband/BandBroadCast.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-row
+    ><v-spacer /><v-col class="my-10" align="center">
+      <video id="localVideo" autoplay playsinline></video>
+      <div>
+        <v-btn class="mx-3" id="testButton" @click="onClickTest"
+          >broadcast</v-btn
+        >
+        <v-btn class="mx-3" id="viewButton" @click="onClickOffer">offer</v-btn>
+      </div> </v-col
+    ><v-spacer />
+  </v-row>
+</template>
+
+<script>
+import * as caller from '@/js/broadcast.js';
+
+export default {
+  methods: {
+    onClickOffer() {
+      caller.createOffer();
+    },
+    onClickTest() {
+      caller.broadcast2().then(function(result) {
+        const localVideo = document.getElementById('localVideo');
+        localVideo.srcObject = result;
+      });
+    },
+  },
+};
+</script>
