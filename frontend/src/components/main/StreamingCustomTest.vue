18d8a495248616d2eccd1de42b3f7c0f0df9a4e7
KimYH-KU <kimyh03160@naver.com>
2021-02-18 14:06:53 +0900

    기능: 공연 신청 페이지에서 공연하기 버튼 추가 - 공연 신청한 밴드장이 공연한 시간에 해당 페이지 접속시 공연이 가능한 페이지로 이동하도록 설정.

--- frontend/src/components/main/StreamingCustomTest.vue
+++ frontend/src/components/main/StreamingCustomTest.vue
@@ -4,7 +4,6 @@
       <video id="remoteVideo" autoplay playsinline></video>
       <div>
         <v-btn class="mx-3" id="watchButton" @click="onClickWatch">watch</v-btn>
-        <v-btn class="mx-3" id="moveButton" @click="onClickMove">move</v-btn>
       </div> </v-col
     ><v-spacer />
   </v-row>
@@ -18,9 +17,6 @@ export default {
     onClickWatch() {
       const remoteVideo = document.getElementById('remoteVideo');
       remoteVideo.srcObject = client.watch();
-    },
-    onClickMove() {
-      this.$router.push('/bandbroadcast');
     },
   },
 };
